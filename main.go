def359ae79947d98f6130c60021b2eb8e8875313
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-22 13:34:17 +0100

    update user information for collect user data

--- main.go
+++ main.go
@@ -12,13 +12,24 @@ var remainingTickets = 50
 	fmt.Printf("We have total of %v tickets and %v are still available. \n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend \n")
 
-var userName string
+var firstName string
+var lastName string
 var userTickets int
+var email int
 
-fmt.Scan(&userName)
+fmt.Printf("Enter your First Name: \n")
+fmt.Scan(&firstName)
 
-userTickets = 2
 
-fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+fmt.Printf("Enter your Last Name: \n")
+fmt.Scan(&lastName)
+
+fmt.Printf("Enter number of tickets: \n")
+fmt.Scan(&userTickets)
+
+fmt.Printf("Enter your email here: \n")
+fmt.Scan(&email)
+
+fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation in your %v", firstName, lastName, userTickets, email)
 
 }
