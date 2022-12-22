ff19c85a471f5117fde70cc13c0637be2b634e12
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-12-22 12:54:52 +0100

    use scan functions to get user data

--- main.go
+++ main.go
@@ -11,4 +11,14 @@ var remainingTickets = 50
 	fmt.Printf("Welcome to %v booking application \n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available. \n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend \n")
+
+var userName string
+var userTickets int
+
+fmt.Scan(&userName)
+
+userTickets = 2
+
+fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
